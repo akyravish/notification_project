a61550743f0210ce03ee576fe6437c5c8172f544
akyravish <info.akshayravish@gmail.com>
2021-06-06 05:55:58 +0530

    make some minor change

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const button  = document.getElementById('button');
+const button = document.getElementById('button');
 const messages = document.querySelector('.messages');
 
-const messageArray =
+const messageArray = [
+	'message 1',
+	'message 2',
+	'message 3',
+	'message 4',
+	'message 5',
+];
 
-const getMessages = () => {
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = randomMeassage();
-}
+const randomMessage = () => {
+	return messageArray[Math.floor(Math.random() * messageArray.length)];
+};
+
+const getMessage = () => {
+	const div = document.createElement('div');
+	div.classList.add('message');
+	div.innerHTML = randomMessage();
+	messages.appendChild(div);
+
+	setTimeout(() => {
+		div.remove();
+	}, 2000);
+};
 
 button.addEventListener('click', () => getMessage());
