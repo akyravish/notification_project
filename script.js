e3443e97d0eaba8fe8371f6a287067eddb310f14
akyravish <info.akshayravish@gmail.com>
2021-06-06 05:42:56 +0530

    create starting variable and add get message funtion

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const button  = document.getElementById('button');
+const messages = document.querySelector('.messages');
 
+const messageArray =
+
+const getMessages = () => {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = randomMeassage();
+}
+
+button.addEventListener('click', () => getMessage());
